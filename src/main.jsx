db4f243ee7d5d256fe357172f0664d3ac3eae955
molniyamaqueen <vatokat1448@gmail.com>
2026-04-15 18:53:35 +0300

    Import index.css for application styling
    Added CSS import for styling.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css' // Мы подключили краску!
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
