13769178cb786c9fde11e96c8c4f9bb567342d43
molniyamaqueen <vatokat1448@gmail.com>
2026-05-01 00:01:04 +0300

    feat: add cases data and arena screen with balance

---
+++ src/data/cases.js
@@ -0,0 +1,16 @@
+export const CASES_DATA = [
+  {
+    id: 'chroma_3',
+    name: 'Chroma 3 Case',
+    price: 2.50,
+    image: 'https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNkHpOSf-ea99457rXWhJ6MQ9H4L_xaTRp7P_JYGpD79_mwtiYksj4Y-6ClT9XvJxy2L-V99SijQXiq0A9MDr6Ldeccl9vMg/256fx256f',
+    rarity: 'gold'
+  },
+  {
+    id: 'revolution',
+    name: 'Revolution Case',
+    price: 5.00,
+    image: 'https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNkHpOSf-ea99457rXWhJ6MQ9H4L_xaTRp7P_JYDhD79_mwtiYksj4Y-6ClT9XvJxy2L-V99SijQXiq0A9MDr6Ldeccl9vMg/256fx256f',
+    rarity: 'red'
+  }
+];
