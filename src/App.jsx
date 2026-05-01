32d97ee3974695ce1f8f31a184f5a32363127e8d
molniyamaqueen <vatokat1448@gmail.com>
2026-05-02 01:24:38 +0300

    fix import

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navigation from './components/Navigation';
 import Preview from './screens/Preview';
 import Arena from './screens/Arena';
 import Profile from './screens/Profile';
+import Season from './screens/Season'; // <--- ДОБАВЬ ВОТ ЭТУ СТРОКУ
 
 const ROUTES = {
   PREVIEW: 'preview',
